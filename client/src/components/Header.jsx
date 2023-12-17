f7857640fd000a1db8f0f16152675666b557f9de
Brandon Garduno <brandgar172@gmail.com>
2023-12-17 16:36:31 -0500

    Downloaded pngs for logo, Adjusted background color for header(nav bar), and tested a few options in logo postion and background color

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className='w-[90px] h-[90px] bg-slate-950'>
+        <img src='/Red_Circle_Logo.png' className= 'p-5' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
