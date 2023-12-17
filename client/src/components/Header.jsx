30f05d4e127e3b95b0d3fd93e18ab071633c0f1b
Brandon Garduno <brandgar172@gmail.com>
2023-12-17 17:01:56 -0500

    Removed Logo and added the nav bar by creating a const called menu and added styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
 function Header() {
+  const menu = [
+    {
+      id:1,
+      name: "HOME",
+    },
+    {
+      id:2,
+      name: "PROJECTS",
+    },
+    {
+      id:3,
+      name: "ABOUT ME",
+    },
+    {
+      id:4,
+      name: "CONTACT",
+    },
+  ];
+
   return (
-    <div>
+    <div className='flex'>
       <div className='w-[90px] h-[90px] bg-slate-950'>
-        <img src='/Red_Circle_Logo.png' className= 'p-5' />
+      </div>
+      <div className='flex gap-14'>
+        {menu.map((item)=>(
+          <div className='cursor-pointer hover:underline font-medium'>
+            <h2>{item.name}</h2>
+          </div>
+        ))}
       </div>
     </div>
   )
