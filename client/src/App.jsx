816ada53278df9a2aa6a12735ef43157e080c648
Brandon Garduno <brandgar172@gmail.com>
2023-12-17 18:28:17 -0500

    Added a profile component alongside a div with my name, Adjust some styling and added a stock image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import SideBar from './components/SideBar'
+import ProfileImg from './components/ProfileImg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,14 @@ function App() {
    <>
     <Header/>
     <SideBar/>
+    <div className='ml-[90px] grid grid-cols-1 md:grid-cols-3'>
+      <div>
+        <div className='text-center'>
+          <h1>Brandon Garduno</h1>
+        </div>
+        <ProfileImg/>
+      </div>
+    </div>
    </>
   )
 }
---
+++ client/src/components/ProfileImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfileImg() {
+  return (
+    <div className='fixed'>
+      <img src="/pexels-tansu-topuzoğlu-16327879.jpg" className='w-screen h-screen object-cover'/>
+    </div>
+  )
+}
+
+export default ProfileImg
