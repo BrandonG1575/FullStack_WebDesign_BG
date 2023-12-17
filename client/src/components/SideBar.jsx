42a56243537c4edf42b50af0d9628db789066d88
Brandon Garduno <brandgar172@gmail.com>
2023-12-17 17:53:39 -0500

    Dowloaded React Icons and Imported them into SideBar.Jsx, added further styling

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {IoLogoYoutube, IoLogoLinkedin, IoLogoInstagram} from "react-icons/io5"
 function SideBar() {
   return (
-    <div className='w-[90px] border-r-[4px] h-screen fixed flex flex-col justify-center item-center'>
+    <div className='w-[90px] border-r-[4px] h-screen fixed flex flex-col justify-around item-center'>
       <h2 className='-rotate-90 font-medium text-xl'>3D Artist</h2>
+      <div className='-rotate-90'>
+      <IoLogoYoutube/>
+      <IoLogoLinkedin/>
+      <IoLogoInstagram/>
+      </div>
     </div>
   )
 }
