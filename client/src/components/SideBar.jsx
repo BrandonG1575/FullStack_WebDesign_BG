2d44d6de00a462b4fffa99c0e1e1825248b33458
Brandon Garduno <brandgar172@gmail.com>
2023-12-17 17:59:06 -0500

    Provided hover animation for Youtube, Instagram, and LinkedIn Icons

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -5,9 +5,9 @@ function SideBar() {
     <div className='w-[90px] border-r-[4px] h-screen fixed flex flex-col justify-around item-center'>
       <h2 className='-rotate-90 font-medium text-xl'>3D Artist</h2>
       <div className='-rotate-90'>
-      <IoLogoYoutube/>
-      <IoLogoLinkedin/>
-      <IoLogoInstagram/>
+      <IoLogoYoutube className='cursor-pointer hover:scale-110 transition-all ease-in-out'/>
+      <IoLogoLinkedin className='cursor-pointer hover:scale-110 transition-all ease-in-out'/>
+      <IoLogoInstagram className='cursor-pointer hover:scale-110 transition-all ease-in-out'/>
       </div>
     </div>
   )
